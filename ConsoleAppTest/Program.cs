a586f97b7877913bb7c3f40bdde064d077c943ec
David Neece <david.neece@bydeluxe.com>
2016-05-27 16:54:28 -0500

    Making something happen.

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleAppTest
 {
@@ -10,6 +6,12 @@ namespace ConsoleAppTest
     {
         static void Main(string[] args)
         {
+            // dispaly some text
+
+            Console.WriteLine(
+                "Howdy!");
+
+            Console.Read();// hold open to see it
         }
     }
 }
